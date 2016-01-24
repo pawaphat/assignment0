ff6fef5bad29213d82433ced5f1bd72415743cca
pawaphat <red_akira@hotmail.com>
2016-01-24 19:19:11 +0700

    hello AI
    Mr.pawaphat sukpadrew 5622791820

---
+++ hello_ai.py
@@ -0,0 +1,7 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ("Pawaphat Sukpadrew 5622791820")
+
+Pawaphat Sukpadrew 5622791820
+>>> 
+>>> 
